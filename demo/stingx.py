fc9260b167c327c04fa25b7235b1fd9a7a6abced
Lhendup Dorji <08250275.sce@rub.edu.bt>
2026-03-18 09:21:05 +0530

    first example of python of string

---
+++ demo/stingx.py
@@ -0,0 +1,11 @@
+print("hello")
+multipleLines= '''this is a stiong 
+that spans multiplenlines.'''
+print(multipleLines)
+a=1
+b=2
+c=a+b
+print (c)
+print(type(c)) #data type of c is int
+c=str(c) # type casting
+print(type(c)) #data type of c is now str
