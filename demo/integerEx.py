52ff0ed334d3b2350e60ac7c51fac3ae7a789ee3
Lhendup Dorji <08250275.sce@rub.edu.bt>
2026-03-18 10:00:47 +0530

    integer example and float example

---
+++ demo/integerEx.py
@@ -0,0 +1,8 @@
+x = 1
+y = 35656222554887711
+z = -3255522
+pi = 3.14
+print (type(pi)) # data type of float is float
+pi = int (pi) # type casting
+print (type(pi)) #data type if pi is now int 
+print (pi)
