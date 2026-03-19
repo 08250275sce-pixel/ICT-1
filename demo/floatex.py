c2b408539bb94a2066d8a4525bd727e468c843bb
Lhendup Dorji <08250275.sce@rub.edu.bt>
2026-03-19 15:30:42 +0530

    this is an example of float

---
+++ demo/floatex.py
@@ -0,0 +1,8 @@
+x = 1.10
+y = 1.0
+z = -35.59
+full_marks = 100
+print(type(full_marks)) # data type of  full marks is int
+full_marks = float (full_marks) # Type castng
+print(type(full_marks)) #data type of full marks is now float
+print (full_marks)
