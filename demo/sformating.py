d1d1e6756d62a708bd4e5b909d45b9a9742d77fc
Lhendup Dorji <08250275.sce@rub.edu.bt>
2026-03-26 15:28:14 +0530

    this type of sformating

--- demo/sformating.py
+++ demo/sformating.py
@@ -1,4 +1,4 @@
-#% operator
+#% operator %s for string %d for integer %f for loat
 print("currently we are having %s class" %"PRG101")
 subject = 'Python'
 module_no = 2.9999999
@@ -7,3 +7,7 @@ print("the moduel number of %s is %.2f" %(subject, module_no))# space% to identi
 print("we are in unit {}".format(2))
 print("{p} is a programming langauge. with the help of {p} we can create various" \
 " applications. {p} is an {i} langauge".format(p='Python',i='interpreted') )#.format to identify {}
+# f string
+topic = "formatting string"
+print(f"we are currently learning {topic}")
+print(f"There are 12 girls and 16 boys in our class.soin total we have {12+16} students")
