a0eb4a65c66a92de5753adeaa0298391eedb91aa
Lhendup Dorji <08250275.sce@rub.edu.bt>
2026-04-30 17:49:43 +0530

    recursion avtivity on 30th Aapril

--- unit 3 function and modularity/recursionlabtask2.py
+++ unit 3 function and modularity/recursionlabtask2.py
@@ -7,7 +7,8 @@ def star(n): #defines the function star and n value tells how may rows will it t
         star(n-1) # recusive call it goes untilit reaches n == 1
         print("* " * n) # it prints n of stars 
         print()# to create the spaces between two rows
-star(4)# calling this starts the function value from 4
+n = int(input("Enter a number of rows of star u want:"))# asking user propmt
+star(n)#calling
 
 
     
